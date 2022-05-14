c8352f76488ab835293799595d4682b476d59a37
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-05-14 14:00:38 +0100

    Argument corrections

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-mopo run_local examples.development --config=examples.config.halfcheetah.off_policy --gpus=1 --trial-gpus=1 --checkpoint-frequency=50
+mopo run_local examples.development --config=examples.config.d4rl.halfcheetah_mixed --gpus=1 --trial-gpus=1 --checkpoint-frequency=50
